63941841c136373a3b3496bc90aeb4ef9a5eb4f2
Todd Thomas <toddthomas@acm.org>
2023-05-26 19:43:57 -0600

    fix: correct MLA citation.

--- Sources/libfib/libfib.swift
+++ Sources/libfib/libfib.swift
@@ -1,6 +1,6 @@
 /// Returns the *n*th Fibonacci number.
 ///
-/// Implementation from David Kopec, _Classic Computer Science Problems in Swift_.
+/// Implementation from David Kopec. _Classic Computer Science Problems in Swift_. Manning, 2018.
 /// - Parameter n: Index of the Fibonacci number to return.
 /// - Returns: The *n*th Fibonacci number.
 public func fib(_ n: UInt) -> UInt {
